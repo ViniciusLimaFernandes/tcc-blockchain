0d8c005d4d1b091869bbcde143a2997af004ea31
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2022-08-25 12:50:54 -0300

    Application architecture impl

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { WalletMultiButton } from "solana-wallets-vue";
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar elevation="4">
+        <h1>Vini APP</h1>
+        <wallet-multi-button dark />
+      </v-app-bar>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import SolanaWallets from "solana-wallets-vue";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import "solana-wallets-vue/styles.css";
+
+const walletOptions = {
+  wallets: [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+  ],
+  autoConnect: true,
+};
+
+loadFonts();
+
+createApp(App)
+  .use(SolanaWallets, walletOptions)
+  .use(router)
+  .use(vuetify)
+  .mount("#app");
