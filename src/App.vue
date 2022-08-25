27d3a0d3819a8665d7a67fa9ba311d93ad6cc3da
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2022-08-25 14:13:02 -0300

    Home features

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@ import { WalletMultiButton } from "solana-wallets-vue";
 </script>
 
 <template>
-  <v-app>
+  <v-app class="home">
     <v-main>
-      <v-app-bar elevation="4">
-        <h1>Vini APP</h1>
+      <v-toolbar flat color="transparent" absolute width="100%">
+        <img class="toolbar-image" src="./assets/logo.png" contain />
         <wallet-multi-button dark />
-      </v-app-bar>
+      </v-toolbar>
     </v-main>
   </v-app>
 </template>
@@ -18,3 +18,16 @@ export default {
   name: "App",
 };
 </script>
+
+<style scoped>
+.home {
+  background: url("./assets/bg.png");
+  background-size: cover;
+  min-height: 100vh;
+}
+
+.toolbar-image {
+  padding-left: 5vh;
+  padding-right: 5vh;
+}
+</style>
