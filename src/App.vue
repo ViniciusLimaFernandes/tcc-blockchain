0f9ab24e6ef09cb130dae766330f9ca4efbae9a3
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2022-08-25 17:15:11 -0300

    Home connections

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,24 @@ import { WalletMultiButton } from "solana-wallets-vue";
 <template>
   <v-app class="home">
     <v-main>
-      <v-toolbar flat color="transparent" absolute width="100%">
-        <img class="toolbar-image" src="./assets/logo.png" contain />
-        <wallet-multi-button dark />
-      </v-toolbar>
+      <v-app-bar flat color="transparent" style="margin-top: 1vh; height: 50vh">
+        <v-app-bar-title
+          ><img class="toolbar-image" src="./assets/logo.png" contain
+        /></v-app-bar-title>
+        <v-toolbar-items style="padding-right: 2vh; align-items: center">
+          <wallet-multi-button dark />
+        </v-toolbar-items>
+      </v-app-bar>
+
+      <v-container class="Home-content">
+        <h1 id="home-title">Blockchain,</h1>
+        <h1 id="home-title">IoT,</h1>
+        <h1 id="home-title">Inovação</h1>
+        <p id="home-subtitle">
+          Mantenha o controle dos seus dispositivos de forma segura e
+          transparente.
+        </p>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -20,6 +34,15 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+  font-family: Inter;
+  src: url("./fonts/Inter-Light.otf");
+}
+
+@font-face {
+  font-family: Inter-Extrabold;
+  src: url("./fonts/Inter-ExtraBold.otf");
+}
 .home {
   background: url("./assets/bg.png");
   background-size: cover;
@@ -29,5 +52,39 @@ export default {
 .toolbar-image {
   padding-left: 5vh;
   padding-right: 5vh;
+}
+
+.Home-content {
+  position: absolute;
+  width: 511px;
+  height: 269px;
+  left: 110px;
+  top: 337px;
+}
+
+#home-title {
+  font-family: "Inter-Extrabold";
+  font-style: bold;
+  font-weight: 800;
+  font-size: 80px;
+  line-height: 97px;
+
+  background: radial-gradient(
+    114.63% 155.16% at 4.44% -42.89%,
+    #ff3eb5 0%,
+    #fefdc2 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+
+#home-subtitle {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  color: #ffffff;
 }
 </style>
