8e9a71cd6bf2fa32119f6080ef1c42ec0472bcae
Vinicius Lima <viniciuslima@MacBook-Air-de-Vinicius.local>
2022-10-16 19:18:26 -0300

    Adding an image to register form

--- src/components/RegisterDeviceForm.vue
+++ src/components/RegisterDeviceForm.vue
@@ -1,42 +1,47 @@
 <template>
   <v-dialog v-model="dialog">
-    <v-card width="350">
-      <v-card-title class="text grey lighten-2">
-        Create a new device
-      </v-card-title>
-      <v-divider />
+    <v-card width="110vh" height="50vh" style="overflow: hidden">
+      <v-row>
+        <v-col width="50vh">
+          <img id="form-image" src="../assets/green-energy.jpg" />
+        </v-col>
 
-      <v-card-text>
-        <v-row>
-          <v-col cols="12" sm="12">
-            <v-text-field
-              variant="underlined"
-              label="KWh price *"
-              hint="Your desired KWh price to be charged"
-              counter="25"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" sm="12">
-            <v-text-field
-              variant="underlined"
-              label="Total ports *"
-              hint="Total ports that your device have"
-              counter="36"
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
-      </v-card-text>
+        <v-col class="form-inputs">
+          <v-card-title class="text grey lighten-2">
+            Device information
+          </v-card-title>
+          <v-divider />
+          <v-card-text>
+            <v-row>
+              <v-col cols="1" sm="12">
+                <v-text-field variant="underlined" label="Name *" hint="Display name" counter="25"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="1" sm="12">
+                <v-text-field variant="underlined" label="KWh price *" hint="Your desired KWh price to be charged"
+                  counter="25"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="1" sm="12">
+                <v-text-field variant="underlined" label="Total ports *" hint="Total ports that your device have"
+                  counter="36" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-card-text>
 
-      <v-divider></v-divider>
+ 
 
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="primary" text @click="dialog = false"> close </v-btn>
-        <v-btn color="primary" text @click="dialog = false"> save </v-btn>
-      </v-card-actions>
+          <v-card-actions id="card-actions">
+            <v-divider></v-divider>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false"> close </v-btn>
+            <v-btn color="primary" text @click="dialog = false"> save </v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+
     </v-card>
   </v-dialog>
 </template>
@@ -50,4 +55,23 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+
+
+#form-image {
+  width: 60vh;
+  height: 50vh;
+}
+
+#card-actions{
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+
+.form-inputs {
+  left: 350;
+  width: 50vh;
+}
+</style>
