9020410b5b09ff15bf5019740e5e6c3d0f5041b8
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2022-10-16 11:42:14 -0300

    Changing create hub style

--- src/components/RegisterDeviceForm.vue
+++ src/components/RegisterDeviceForm.vue
@@ -1,6 +1,6 @@
 <template>
   <v-dialog v-model="dialog">
-    <v-card width="700">
+    <v-card width="350">
       <v-card-title class="text grey lighten-2">
         Create a new device
       </v-card-title>
@@ -8,25 +8,21 @@
 
       <v-card-text>
         <v-row>
-          <v-col cols="12" sm="6">
+          <v-col cols="12" sm="12">
             <v-text-field
-              label="Device name *"
-              hint="Public name"
+              variant="underlined"
+              label="KWh price *"
+              hint="Your desired KWh price to be charged"
               counter="25"
             ></v-text-field>
           </v-col>
-          <v-col cols="12" sm="6">
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="12">
             <v-text-field
-              label="Device code *"
-              hint="Device UUID"
-              counter="36"
-              required
-            ></v-text-field>
-          </v-col>
-          <v-col cols="20">
-            <v-text-field
-              label="Another field *"
-              hint="Device UUID"
+              variant="underlined"
+              label="Total ports *"
+              hint="Total ports that your device have"
               counter="36"
               required
             ></v-text-field>
@@ -38,8 +34,8 @@
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary" text @click="dialog = false"> save </v-btn>
         <v-btn color="primary" text @click="dialog = false"> close </v-btn>
+        <v-btn color="primary" text @click="dialog = false"> save </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
